6f6695b656a0c44182c2ad0bd7a5cd8f0e10fb10
Szemin <116577945+Szemin@users.noreply.github.com>
2023-05-16 16:15:45 +0200

    Is more vowels or Consonants algorithm

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -8,15 +8,29 @@ namespace TestProject1
         {            
             Assert.AreEqual(Algorytm1.Repleace("ci¹gnik", '¹', 'u'), "ciugnik");
             Assert.AreEqual(Algorytm1.Repleace("grzyb", 'y', '6'), "grz6b");
-            Assert.AreEqual(Algorytm1.Repleace("fotowoltaika", 'o', ' '), "f t w ltaika");
-            
+            Assert.AreEqual(Algorytm1.Repleace("fotowoltaika", 'o', ' '), "f t w ltaika");            
         }
         [Test]
         public void Test_IndexOf()
         {
-            Assert.AreEqual(IndexOf.Indexof("dupa", 'd'), 0);
+            var result = IndexOf.Indexof("dupa", 'd');
+            Assert.AreEqual(result, 0);
             Assert.AreEqual(IndexOf.Indexof("klocek", 'o'), 2);
-            Assert.AreEqual(IndexOf.Indexof("minus", '2'), -1);
+            Assert.AreEqual(IndexOf.Indexof("minus", '2'), -1);          
+        }
+
+        [Test]
+        public void Test_Is_MoreVowels()
+        {
+            //aaa
+            //arrange
+            var ini = new Vowels_and_consonants();
+            //act
+            var result = ini.IsMoreVowels("aaab");
+            var result2 = ini.IsMoreVowels("bbbba");
+            //assert
+            Assert.IsTrue(result);
+            Assert.IsFalse(result2);
         }
     }
 }
---
+++ TestProject1/vowels and consonants.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class Vowels_and_consonants
+    {
+        public bool IsMoreVowels(string word)
+        {
+            //char[] znaki = word.ToCharArray();
+            //char[] chars = new char[] { 'a','e','i','o','u'};
+            string vowels = "aeiou";   //string z samogłoskami
+            int howManyVowels = 0;    //zmienna zliczająca samo
+            int howManyConsonants = 0;  //zmienna zliczająca spół 
+
+            for (int i = 0; i < word.Length; i++)
+            {
+               char currentChar = word[i];    //tablica z konkretnym znakiem przypisując do niej słowo z indeksem od i
+                if(char.IsLetter(currentChar))   //sprawdzanie czy char to litera z tablicy znaków
+                {
+                    if(vowels.Contains(currentChar))   //jeżeli zmienna samogłoski zawiera samogłoskę licznik += 1
+                    {
+                        howManyVowels++;
+                    }
+                    else                                //jeżeli spółgłoska zwiększa licznik += 1
+                    {
+                        howManyConsonants++;
+                    }
+                }
+            }
+            return howManyVowels > howManyConsonants;    //zwraca prawdę gdy więcej jest samo niż spół
+
+        }
+    }
+}
