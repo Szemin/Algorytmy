f4d1d94b0faaa13fdf917e244c3f812a6402a32a
Szemin <116577945+Szemin@users.noreply.github.com>
2023-05-16 15:16:59 +0200

    indexOf algorithm + test

---
+++ TestProject1/IndexOf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class IndexOf
+    {
+        //algorytm zwracający indeks na którym jest konkretny znak
+        public static int Indexof(string napis,char znak)
+        {
+            for (int i = 0; i < napis.Length; i++)
+            {
+                if (napis[i]==znak)
+                {
+                    return i;                    
+                }
+            }
+            return -1;
+        }
+    }
+}
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -4,10 +4,19 @@ namespace TestProject1
     {       
 
         [Test]
-        public void Test1()
-        {
+        public void Test_Repleace()
+        {            
+            Assert.AreEqual(Algorytm1.Repleace("ci¹gnik", '¹', 'u'), "ciugnik");
+            Assert.AreEqual(Algorytm1.Repleace("grzyb", 'y', '6'), "grz6b");
+            Assert.AreEqual(Algorytm1.Repleace("fotowoltaika", 'o', ' '), "f t w ltaika");
             
-           Assert.AreEqual(Algorytm1.Repleace("ci¹gnik", '¹', 'u'), "ciugnik");
+        }
+        [Test]
+        public void Test_IndexOf()
+        {
+            Assert.AreEqual(IndexOf.Indexof("dupa", 'd'), 0);
+            Assert.AreEqual(IndexOf.Indexof("klocek", 'o'), 2);
+            Assert.AreEqual(IndexOf.Indexof("minus", '2'), -1);
         }
     }
 }
