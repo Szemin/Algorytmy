10b21a5b2d5653a1b514108e74d1677ae49755d3
Szemin <116577945+Szemin@users.noreply.github.com>
2023-05-17 17:26:58 +0200

    RepleaceEvenOdd boolean method added

--- TestProject1/Algorytmy/ChangingElementsOfArray.cs
+++ TestProject1/Algorytmy/ChangingElementsOfArray.cs
@@ -15,7 +15,7 @@ namespace TestProject1.Algorytmy
             {
                 if (tab[i] % 2 == 0)  // if((tab[i] & 1) == 0)
                 {
-                    return i;
+                    return i;   //jeżeli parzysta zwraca indeks od i
                 }
             }
             return-1;
@@ -24,9 +24,9 @@ namespace TestProject1.Algorytmy
         {
             for (int i = 0; i < tab.Length; i++)
             {
-                if (tab[i] % 2 == 1)  // if((tab[i] & 1) != 0)
+                if (tab[i] % 2 == 1)  // if((tab[i] & 1) != 0)  
                 {
-                    return i;
+                    return i;      //jeżeli nieparzysta zwraca indeks od i
                 }
             }
             return -1;
@@ -34,12 +34,24 @@ namespace TestProject1.Algorytmy
 
         public void Repleace(int[] tab, int index1, int index2) 
         {
-            if (index1 < 0 || index1 >= tab.Length || index2 < 0 || index2 >= tab.Length) return;
+            //sprawdzanie czy index1 <0 albo >= długości tablicy tak samo index2
+            if (index1 < 0 || index1 >= tab.Length || index2 < 0 || index2 >= tab.Length) return; 
             
-            int temp = tab[index1];
+            int temp = tab[index1];    //zamiana wartości z index1 na index2 i na odwrót
             tab[index1] = tab[index2];
-            tab[index2] = temp;
-            
+            tab[index2] = temp;            
+        }
+
+        public bool RepleaceEvenOdd(int[] tab)
+        {
+            int even = IsFistEven(tab);   //przypisywanie do even funkcji sprawdzającej czy parzysta na podstawie tablicy
+            int odd = IsFistOdd(tab);   //przypisywanie do odd funkcji sprawdzającej czy nieparzysta na podstawie tablicy
+            if (even == -1 || odd == -1)
+            {
+                return false;   //jeżeli nie to zwraca -1
+            }
+            Repleace(tab, even, odd);    //zmiana miejsc na indeksach 
+            return true;
         }
 
     }
